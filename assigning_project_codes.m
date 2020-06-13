51797a0e82075d0301386c3b9904aedd17e7a6e6
Vasista <adupa@iitg.ac.in>
2020-06-14 01:39:29 +0530

    Updated
    Mostly removed the bug but didnot store the keywords of a specific project. If we get that, then this file is mostly done.

--- assigning_project_codes.m
+++ assigning_project_codes.m
@@ -7,29 +7,41 @@ clc;clear
 %%
 %Need to write every thing in generic form. I am writing for this particular example
 proj = 'proj';
-txt = [txt(:,2) txt(:,5:14)]
+txt = [txt(:,2) txt(:,5:end)]
 
 txt2 = txt2(2:end,end);
 [N_students,total_choices] = size(txt);
 
-
+clc;
 %for i = 2: N_students   % First column is titles
-i = 2;
-    for j = 2:total_choices
-        %txt_char(i,j) = {char(txt(i,j));}
-        for k = 1:length(txt2)
-            result(k) = ~cellfun('isempty',regexpi(txt(i,j),txt2(k)));
-            if result(k) == 1
-                storing(j) = k;
-                repeated = cellstr(num2str((sum(storing(:)== k))));
-                txt(2,j) = append(txt2(k),proj,repeated);
-                %continue
-                
-            end
-            
+i = 2;   % For first student, It is written to save the project names.
+
+for j = 2:total_choices
+    
+    for k = 1:length(txt2)
+        
+        result(k) = ~cellfun('isempty',regexpi(txt(i,j),txt2(k)));
+        
+        if result(k) == 1
+            storing(j) = k;
+            repeated = cellstr(num2str((sum(storing(:)== k))));
+            append(txt2(k),proj,repeated)
+            choic_no = j - 1
+            txt(2,j) = append(txt2(k),proj,repeated);
+            break
         end
         
     end
+    if strlength(txt(2,j)) > 20
+        msg = sprintf('It is observed that the professor name is misspelled or used a surname insted of last name')
+        resolution = sprintf( 'go to choice no: %f and enter the correct name',choic_no+1)
+        prompt{1} = ('Enter the correct name of the professor');
+        name = 'Updated Professor Name';
+        new_name=(inputdlg(prompt{1},name, [1 30]));
+        error('Error in Professor name')
+    end
+    clear k
+    clear repeated
     
-    txt (2,2)
-    
+end
+clc;
