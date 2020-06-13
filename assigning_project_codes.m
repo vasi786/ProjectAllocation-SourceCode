780b16394e398624ce61d56c4a22a2f92e248774
Vasista <adupa@iitg.ac.in>
2020-06-13 23:25:26 +0530

    Updated

--- assigning_project_codes.m
+++ assigning_project_codes.m
@@ -1,29 +1,34 @@
 clc;clear
 
 [num,txt] = xlsread('students-choices.xlsx');
-txt = string(txt);
+%txt = string(txt);
 
 [num2,txt2] = xlsread('prof_list-keywords.xlsx');
 %%
 %Need to write every thing in generic form. I am writing for this particular example
-
+proj = 'proj';
 txt = [txt(:,2) txt(:,5:14)]
 
 txt2 = txt2(2:end,end);
 [N_students,total_choices] = size(txt);
 
+
 for i = 2: N_students   % First column is titles
     for j = 2:total_choices
-        txt_char(i,j) = {char(txt(i,j));}
+        %txt_char(i,j) = {char(txt(i,j));}
         for k = 1:length(txt2)
-            result(k) = ~cellfun('isempty',regexpi(txt_char(i,j),txt2(k)));
-            if result(k) == 1 
+            result(k) = ~cellfun('isempty',regexpi(txt(i,j),txt2(k)));
+            if result(k) == 1
+                storing(j) = k;
+                repeated = cellstr(num2str((sum(storing(:)== k))));
+                txt(2,j) = append(txt2(k),proj,repeated);
+                %continue
                 
+            end
+            
         end
         
     end
     
-end
-
-txt (2,2)
-
+    txt (2,2)
+    
