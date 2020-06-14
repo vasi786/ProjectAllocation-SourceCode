7b51438e8ecc80d4bbad3f2a556907beaab71709
Vasista <adupa@iitg.ac.in>
2020-06-14 11:02:29 +0530

    totally updated code for 10 students,78 projects
    Fixed all bugs and for now it runs for 10 students as the excel sheet created is for 10 students. and all projects 78

--- assigning_project_codes.m
+++ assigning_project_codes.m
@@ -5,7 +5,8 @@ clc;clear
 
 [num2,txt2] = xlsread('prof_list-keywords.xlsx');
 %%
-%Need to write every thing in generic form. I am writing for this particular example
+%Need to write every thing in generic form. I am writing for this
+%particular example  taken care of just 10 students
 proj = 'proj';
 txt = [txt(:,2) txt(:,5:end)]
 
@@ -13,43 +14,48 @@ txt2 = txt2(2:end,end);
 [N_students,total_choices] = size(txt);
 
 clc;
-%for i = 2: N_students   % First column is titles
-i = 2;   % For first student, It is written to save the project names.
-
-for j = 2:total_choices
+for i = 2: N_students   % First column is titles
+    %    i = 2;   % For first student, It is written to save the project names.
     
-    for k = 1:length(txt2)
+    for j = 2:total_choices % first column is titles
         
-        result(k) = ~cellfun('isempty',regexpi(txt(i,j),txt2(k)));
-        
-        if result(k) == 1
-            storing(j) = k;
-            repeated = cellstr(num2str((sum(storing(:)== k))));
-            append(txt2(k),proj,repeated)
-            choic_no = j - 1
-            txt(2,j) = append(txt2(k),proj,repeated);
-            break
+        for k = 1:length(txt2)
+            
+            result(k) = ~cellfun('isempty',regexpi(txt(i,j),txt2(k)));
+            
+            if result(k) == 1
+                storing(j) = k;
+                repeated = cellstr(num2str((sum(storing(:)== k))));
+                append(txt2(k),proj,repeated)
+                choic_no = j - 1;
+                txt(i,j) = append(txt2(k),proj,repeated);
+                break
+            end
         end
-        
-    end
-    
-    if strlength(txt(2,j)) > 20
-        Message = sprintf('It is observed that the professor name is misspelled or used a surname instead of last name')
-        Fix = sprintf( 'go to choice no: %f and check for the correct/actual name',choic_no+1)
-        Example = sprintf('If the name is prof A.PR. Thorne, then consider Thorne')
-        prompt{1} = ('Enter the correct name of the professor');
-        prompt{2} = ('Specify the row number of that professor in the prof_list-keywords.xlsx file');
-        name1 = 'Updated Professor Name';
-        name2 = 'Row Number';
-        new_name=(inputdlg(prompt{1},name1, [1 50]));
-        Row_num=(inputdlg(prompt{2},name2, [1 50]));
-        location = char(append('B',Row_num));
-        xlswrite('prof_list-keywords.xlsx',new_name,1,location)
-        error('Error in Professor name')
+        if i ==2
+            
+        elseif strlength(txt(i,j)) > 20
+            Message = sprintf('It is observed that the professor name is misspelled or used a surname \ninstead of last name')
+            project_name = string(txt(2,j));
+            Fix = sprintf('The professor name and project is as follows: \n%s',project_name)
+            Example = sprintf('If the name of the prof A.PR. Thorne, then consider Thorne')
+            prompt{1} = ('Enter the correct name of the professor');
+            prompt{2} = ('Specify the row number of that professor in the prof_list-keywords.xlsx file');
+            name1 = 'Updated Professor Name';
+            name2 = 'Row Number';
+            new_name=(inputdlg(prompt{1},name1, [1 50]));
+            Row_num=(inputdlg(prompt{2},name2, [1 50]));
+            location = char(append('B',Row_num));
+            xlswrite('prof_list-keywords.xlsx',new_name,1,location)
+            error('Error in Professor name')
+        end
     end
     
     clear k
     clear repeated
-    
+    clear result
+    clear storing
+    clear repeated
+    clear choic_no
 end
 clc;
