df815f37fd9a7976593f4b7ffdbef9c219e712da
Vasista <adupa@iitg.ac.in>
2020-06-16 16:08:22 +0530

    updated inputs for the function
    txt3 is updated while assigning choices, while original_proj_list remains constant.

--- getprojname.m
+++ getprojname.m
@@ -1,8 +1,8 @@
-function [project_Name] = getprojname(project_Keyword,txt3)
+function [project_Name] = getprojname(project_Keyword,original_proj_list)
 % function supplies project name for project keyword
 % Note: txt3 should be loaded in memory for succesfull operation
   for i=1:length(txt3)
-      if strcmpi(project_Keyword,txt3(i,2))
+      if strcmpi(project_Keyword,original_proj_list(i,2))
           project_Name = txt3(i,1)
       end
   end
