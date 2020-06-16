87bc0e1f4df1d3dc949dd6c53f0678e3a7b9d3ff
Venu Angirekula <52244272+Venu-41@users.noreply.github.com>
2020-06-16 10:23:54 +0530

    Create getprojname.m

---
+++ getprojname.m
@@ -0,0 +1,9 @@
+function [project_Name] = getprojname(project_Keyword,txt3)
+% function the supplies project name for project keyword
+% Note: txt3 should be loaded in memory for succesfull operation
+  for i=1:length(txt3)
+      if strcmpi(project_Keyword,txt3(i,2))
+          project_Name = txt3(i,1)
+      end
+  end
+end
