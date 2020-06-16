fe4d1574b92c2d0664305080772093aa29b7c057
Vasista <adupa@iitg.ac.in>
2020-06-16 10:25:49 +0530

    Update getprojname.m

--- getprojname.m
+++ getprojname.m
@@ -1,5 +1,5 @@
 function [project_Name] = getprojname(project_Keyword,txt3)
-% function the supplies project name for project keyword
+% function supplies project name for project keyword
 % Note: txt3 should be loaded in memory for succesfull operation
   for i=1:length(txt3)
       if strcmpi(project_Keyword,txt3(i,2))
