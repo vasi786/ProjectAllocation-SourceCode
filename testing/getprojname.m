2509a8d23fa29794c781a5f435ad8adbdffc9f27
vasi786 <adupa@iitg.ac.in>
2020-07-24 00:15:16 +0530

    Remove testing

--- testing/getprojname.m
+++
@@ -1,9 +0,0 @@
-function [project_Name] = getprojname(project_Keyword,original_proj_list)
-% function supplies project name for project keyword
-% Note: txt3 should be loaded in memory for succesfull operation
-  for i=1:length(original_proj_list)
-      if strcmpi(project_Keyword,original_proj_list(i,2))
-          project_Name = original_proj_list(i,1);
-      end
-  end
-end
